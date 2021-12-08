2e4d395ebd4d6d64ed9cc5afd9cfbccd8150d320
Esteban <encinaesteban27@gmail.com>
2021-12-08 14:50:34 -0300

    Custom settings and next page

--- 13-Scrapy Project/quotes_scraper/quotes_scraper/quotes_scraper/spiders/quotes.py
+++ 13-Scrapy Project/quotes_scraper/quotes_scraper/quotes_scraper/spiders/quotes.py
@@ -3,12 +3,28 @@ import scrapy
 #Titulo = //h1/a/text()
 #Citas = //span[@class = "text" and @itemprop = "text"]/text()
 #Top ten tags= //div[contains(@class, "tags-box")]//span[@class="tag-item"]/a/text()
+#next page button = //li[@class = "next"]/a/@href
 
 class QuotesSpider(scrapy.Spider):
     name = 'quotes'
     start_urls = [
         'http://quotes.toscrape.com/page/1/',
     ]
+    #Export to json file
+    custom_settings = {
+        'FEEDS': {
+            'quotes.json': {
+                'format': 'json',
+                'encoding': 'utf8',
+                'store_empty': False,
+                'fields': None,
+                'indent': 4,
+                'item_export_kwargs': {
+                    'export_empty_fields': True,
+                },
+            },
+        },
+    }
 
     def parse(self, response):
         title = response.xpath('//h1/a/text()').get()
@@ -19,4 +35,8 @@ class QuotesSpider(scrapy.Spider):
         yield {'Title': title, 
                 'Citas': quotes, 
                 'Top ten tags': top_ten_tags}
+        #Button to next page
+        next_page_button_link = response.xpath('//li[@class = "next"]/a/@href').get()
+        if next_page_button_link:
+            yield response.follow(next_page_button_link, callback=self.parse)
 
